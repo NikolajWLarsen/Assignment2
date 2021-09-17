430f024de212cf53f100ac887847ff86aad2ab9c
ArcanicKnight <alsu@itu.dk>
2021-09-17 13:36:04 +0200

    implemented Student

---
+++ Assignment2/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Assignment2
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+        public Status status { get => getStatus(); }
+        DateTime StartDate { get; set; }
+        DateTime EndDate { get; set; }
+        public DateTime GraduationDate { get; set; }
+        
+        public Student(int Id)
+        {
+            this.id = Id;
+        }
+        public Status getStatus()
+        {
+            
+            var time = DateTime.Now;
+            if ((time - StartDate).TotalDays < 121)
+            {
+                return Status.New;
+            }
+            else if ((time - StartDate).TotalDays > 121 && time < EndDate)
+            {
+                return Status.Active;
+            }
+            else
+            {
+                return Status.Graduated;}
+        }
+
+        public string ToString()
+        {
+            return "Student#" + id + ": " + GivenName + " " + Surname + " is " + status;
+        }
+        
+        
+
+    }
+
+    public enum Status
+    {
+        New, Active, Dropout, Graduated
+    }
+
+    
+}
